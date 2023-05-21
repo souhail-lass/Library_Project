5015c8bdfdb9455d892e210b536afb691a201a62
Souhail Lassoued <76921303+souhail-lass@users.noreply.github.com>
2023-05-21 09:36:16 +0100

    'done'

--- main.js
+++ main.js
@@ -17,75 +17,75 @@ let myLibrary = [];
 // the constructor...
 
 function Book(title, author, pages, isRead) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.isRead = isRead;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
 // Add to library & Render it
 
 const addBookToLibrary = () => {
-	let title = document.querySelector("#title").value;
-	let author = document.querySelector("#author").value;
-	let pages = document.querySelector("#pages").value;
-	let isRead = document.querySelector("#isRead").checked;
-	let newBook = new Book(title, author, pages, isRead);
-	myLibrary.push(newBook);
-	render();
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let isRead = document.querySelector("#isRead").checked;
+  let newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+  render(newBook);
 };
 
 // Open book form
 
 const openAddBook = () => {
-	addBookForm.reset();
-	addBookForm.classList.add("active");
-	overlay.classList.add("active");
+  addBookForm.reset();
+  addBookForm.classList.add("active");
+  overlay.classList.add("active");
 };
 
 // Close book form
 
 const closeAddBook = () => {
-	addBookForm.classList.remove("active");
-	overlay.classList.remove("active");
+  addBookForm.classList.remove("active");
+  overlay.classList.remove("active");
 };
 
-const render = () => {
-	booksGrid.innerHTML = "";
-	for (let i = 0; i < myLibrary.length; i++) {
-		let libraryEL = myLibrary[i];
-		let book = document.createElement("div");
-		document.body.appendChild(book);
-		book.innerHTML = `
-        <p>" ${libraryEL.title} "</p>
-        <p>${libraryEL.author}</p>
-        <p>${libraryEL.pages} pages</p>
-        <div class="button-grp">
-            <button id ="readButton" onclick="toggleReadState(${i})" class=${
-			libraryEL.isRead ? "btn-light-green" : "btn-light-red"}>  ${libraryEL.isRead ? "Read" : "Not Read"}</button>
-            <button id ="removeButton" onclick="removeBook(${i})" >Remove</button>
-        </div>`;
-		book.className = "styleBook";
-		let readButton = book.querySelector("#readButton");
-		booksGrid.appendChild(book);
-		closeAddBook();
-	}
+const render = (newBook) => {
+  booksGrid.innerHTML =
+    booksGrid.innerHTML +
+    `
+		<div class="styleBook">
+			<p>" ${newBook.title} "</p>
+			<p>${newBook.author}</p>
+			<p>${newBook.pages} pages</p>
+			<div class="button-grp">
+				<button id ="readButton" onclick="toggleReadState(this)" class=${
+          newBook.isRead ? "btn-light-green" : "btn-light-red"
+        }>  ${newBook.isRead ? "Read" : "Not Read"}</button>
+				<button id ="removeButton" onclick="removeBook(this)" >Remove</button>
+			</div>
+		</div>`;
+  let storeData=JSON.stringify(newBook);
+  localStorage.setItem(`"Book ${i+1}"`,`${storeData}`);
+  closeAddBook();
 };
 
-const toggleReadState = (i) => {
-	myLibrary[i].isRead = !myLibrary[i].isRead; 
-    render();
+const toggleReadState = (e) => {
+  if (e.className == "btn-light-green") {
+    e.className = "btn-light-red";
+    e.innerText = "Not Read";
+  } else {
+    e.className = "btn-light-green";
+    e.innerText = "Read";
+  }
 };
 
-const removeBook = (i) => {
-    myLibrary.splice(i,1);       
-    render();
-};
+const removeBook = (e) => {e.parentNode.parentNode.remove()};
 
 addBookBtn.onclick = openAddBook;
 overlay.onclick = closeAddBook;
 
 addBookForm.addEventListener("submit", (event) => {
-	event.preventDefault();
-	addBookToLibrary();
+  event.preventDefault();
+  addBookToLibrary();
 });
